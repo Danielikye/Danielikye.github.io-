7d9dbac9ff5c97b683465de6f025a1e97450f56b
Iwuegbulem Daniel Ikechukwu <122985416+Danielikye@users.noreply.github.com>
2023-12-06 22:49:17 +0100

    Create script.js
    updated

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function () {
+    // Code to run after the DOM has fully loaded
+    console.log('DOM is ready');
+
+    // You can add more JavaScript functionality here.
+});
